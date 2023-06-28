7b463b76636cbcfb6663bd3ee1cb490fa5950b7c
GeetikaSharma-TCH <geetika@technocratshorizons.com>
2023-06-28 18:37:41 +0530

    changes in file

--- pages/SignIn/signIn.jsx
+++ pages/SignIn/signIn.jsx
@@ -3,9 +3,9 @@ const SignIn = () => {
     const getData = () => {
         console.log('wwww')
         // const url  =
-        // 'https://accounts.zoho.com/oauth/v2/auth?response_type=code&client_id=1000.0TA4E9SZNQYLO5AWXP2HASB597VRXA&scope=ZohoProjects.portals.ALL&redirect_uri=http://localhost:3001/SignIn/signIn&prompt=consent'
+        // 'https://accounts.zoho.com/oauth/v2/token?response_type=code&client_id=1000.W0NPL9G80Q1AI83G3T3UYWP3LRI1AU&scope=ZohoProjects.portals.ALL&redirect_uri=http://localhost:3001/SignIn/signIn&prompt=consent'
 
-        // axios.get(url)
+        // axios.post(url)
         // .then((res) => console.log('res', res))
     }
     return (  
---
+++ pages/SignIn/token.jsx
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const Token = () => {
+    
+    return (  
+        <div>
+            <button>Sign In with Facebook</button>
+        </div>
+    );
+}
+ 
+export default Token;
